377ab25199db778db85f3cb0dc01e12308ed9022
neo_cloud <1786546913@qq.com>
2018-10-05 16:20:28 +0800

    update

--- src/chapter2/AllOnesGA.java
+++ src/chapter2/AllOnesGA.java
@@ -15,7 +15,8 @@ package chapter2;
  * example is very simple: if the fitness score (ie, number of ones) is the same
  * as the length of the chromosome (ie, we're all ones), we're done!
  * 这个问题的简单性使得GeneticAlgorithm类的"calcFitness"(计算适应度函数)方法十分简单。我们仅仅需要数染色体中1的数量，使用这个作为适应度函数。
- * 相似的,这个问题中的"isTerminationConditionMet"
+ * 相似的,这个问题在GeneticAlgorithm class中的"isTerminationConditionMet"(是否达到终止条件)方法也很简单，
+ * 只要适应度函数(即染色体中1的个数)和染色体长度一致时，即是染色体中基因全为1的情况，此时迭代停止
  * 
  * @author bkanber
  *
@@ -23,10 +24,15 @@ package chapter2;
 public class AllOnesGA {
 
 	public static void main(String[] args) {
-		// Create GA object
+		// Create GA object(初始化遗传算法，作为应用程序的起点)
 		GeneticAlgorithm ga = new GeneticAlgorithm(100, 0.001, 0.95, 2);
+		// 使用典型的参数值，种群规模=100,变异率=0.01,交叉率=0.95,精英计数为0(即暂时禁用精英策略)
 
 		// Initialize population
+		/*
+		 * 初始化一个潜在解构成的种群,这通常是随机的，但偶尔也可以采用系统化的方法使其变得更好，
+		可以利用对搜索空间的一致信息来初始化种群
+		*/
 		Population population = ga.initPopulation(50);
 
 		// Evaluate population
@@ -38,11 +44,10 @@ public class AllOnesGA {
 		/**
 		 * Start the evolution loop
 		 * 
-		 * Every genetic algorithm problem has different criteria for finishing.
-		 * In this case, we know what a perfect solution looks like (we don't
-		 * always!), so our isTerminationConditionMet method is very
-		 * straightforward: if there's a member of the population whose
-		 * chromosome is all ones, we're done!
+		 * Every genetic algorithm problem has different criteria for finishing. In this
+		 * case, we know what a perfect solution looks like (we don't always!), so our
+		 * isTerminationConditionMet method is very straightforward: if there's a member
+		 * of the population whose chromosome is all ones, we're done!
 		 */
 		while (ga.isTerminationConditionMet(population) == false) {
 			// Print fittest individual from population
@@ -62,9 +67,9 @@ public class AllOnesGA {
 		}
 
 		/**
-		 * We're out of the loop now, which means we have a perfect solution on
-		 * our hands. Let's print it out to confirm that it is actually all
-		 * ones, as promised.
+		 * We're out of the loop now, which means we have a perfect solution on our
+		 * hands. Let's print it out to confirm that it is actually all ones, as
+		 * promised.
 		 */
 		System.out.println("Found solution in " + generation + " generations");
 		System.out.println("Best solution: " + population.getFittest(0).toString());
--- src/chapter2/Individual.java
+++ src/chapter2/Individual.java
@@ -18,23 +18,47 @@ package chapter2;
  * @author bkanber
  *
  */
+
+/**
+ * "Individual"个体代表单个的候选解决方案,关于个人的核心信息是它的“Chromosome染色体”，表示当前问题的可能解决方案的编码，
+ * 染色体可以是字符串，数组，列表等 - 在这个类中，染色体是整数数组。
+ * 
+ * 染色体中的单个位置称为基因，这些是可被操作或突变的溶液的原子片段。 当染色体是一个字符串时，每个字符或一组字符可以被视为是一个基因。
+ * 
+ * 个体也有“适应度”分数; 代表这个个体作为问题的解决方案的好坏。 适应度函数的定义根据当前的问题而变化。
+ * @author cloud
+ *
+ */
+
+
 public class Individual {
-	private int[] chromosome;
-	private double fitness = -1;
+	private int[] chromosome; //定义染色体
+	private double fitness = -1; //定义适应度值
+	
+	/**
+	 * Individual类代表一个候选解，主要负责存储和操作一条染色体。
+	 * 请注意Individual类有两个构造方法：
+	 * 一个构造方法接收一个整数(代表染色体的长度)，在初始化对象时将创建一条随机的染色体
+	 * 一个构造方法接受一个整数数组，用它作为染色体
+	 * @author cloud
+	 *
+	 */
 
 	/**
 	 * Initializes individual with specific chromosome
-	 * 
-	 * @param chromosome
-	 *            The chromosome to give individual
+	 * 用特定染色体初始化个体
+	 * @param chromosome 
+	 * 			     染色体
+	 *            The chromosome to give individual 赋给个体的染色体
 	 */
 	public Individual(int[] chromosome) {
-		// Create individual chromosome
+		// Create individual chromosome 创建个体染色体
 		this.chromosome = chromosome;
 	}
 
 	/**
 	 * Initializes random individual.
+	 * 初始化随机个体
 	 * 
 	 * This constructor assumes that the chromosome is made entirely of 0s and
 	 * 1s, which may not always be the case, so make sure to modify as
@@ -42,9 +66,13 @@ public class Individual {
 	 * simply picking random zeroes and ones, which also may not be the case
 	 * (for instance, in a traveling salesman problem, this would be an invalid
 	 * solution).
+	 * 这个构造函数假设染色体完全由0和1组成，这可能并非总是如此，
+	 * 因此请务必根据需要进行修改。 这个构造函数还假设“随机”染色体意味着简单地选择随机0和1，
+	 * 这也可能不是这种情况（例如，在旅行商问题中，这将是无效的解决方案）。
 	 * 
 	 * @param chromosomeLength
-	 *            The length of the individuals chromosome
+	 * 			      染色体长度
+	 *            The length of the individuals chromosome   染色体个体长度
 	 */
 	public Individual(int chromosomeLength) {
 
@@ -61,6 +89,7 @@ public class Individual {
 
 	/**
 	 * Gets individual's chromosome
+	 * 返回染色体个体
 	 * 
 	 * @return The individual's chromosome
 	 */
@@ -79,7 +108,8 @@ public class Individual {
 
 	/**
 	 * Set gene at offset
-	 * 
+	 * offset用于传入位于染色体上的基因位置索引
+	 * gene代表基因位上的值
 	 * @param gene
 	 * @param offset
 	 * @return gene
